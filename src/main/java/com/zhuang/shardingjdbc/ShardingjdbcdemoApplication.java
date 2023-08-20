10209ca4a3274d8c239dfefc096f803f3d9c76a1
Kang-xiao-zhuang <2247830091@qq.com>
2023-08-20 21:10:28 +0800

    分库分表启动类

---
+++ src/main/java/com/zhuang/shardingjdbc/ShardingjdbcdemoApplication.java
@@ -0,0 +1,15 @@
+package com.zhuang.shardingjdbc;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.zhuang.shardingjdbc.mapper")
+public class ShardingjdbcdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingjdbcdemoApplication.class, args);
+    }
+
+}
